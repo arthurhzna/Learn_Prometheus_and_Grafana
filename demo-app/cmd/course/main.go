17d01f4c56e546489f13f7fafca315411ee6ae75
arthurhzna <arthurhozana123@gmail.com>
2026-01-11 14:27:08 +0700

    feat: cmd

---
+++ demo-app/cmd/course/main.go
@@ -0,0 +1,7 @@
+2.1s 
+✔ Container demo-app-postgres-1    Started                                                                                    2.2s 
+arthurhzna@arthurhzna-HYPE-5-AMD-X3:~/Downloads/grinding/software-instrument/Learn_Prometheus_and_Grafana/demo-app$ make course/server
+go run cmd/course/main.go server start --config course/conf/server.yaml --migration course/migrations
+stat cmd/course/main.go: no such file or directory
+make: *** [Makefile:67: course/server] Error 1
+arthurhzna@arthurhzna-HYPE-5-AMD-X3:~/Downloads/grinding/software-instrument/Learn_Prometheus_and_Grafana/demo-app$ 
