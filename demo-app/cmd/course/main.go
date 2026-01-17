644d19162e92b72a69f025a7a468aec4753b7b5f
arthurhzna <arthurhozana123@gmail.com>
2026-01-17 15:51:43 +0700

    feat: add interceptor

--- demo-app/cmd/course/main.go
+++ demo-app/cmd/course/main.go
@@ -1,7 +1,14 @@
-2.1s 
-✔ Container demo-app-postgres-1    Started                                                                                    2.2s 
-arthurhzna@arthurhzna-HYPE-5-AMD-X3:~/Downloads/grinding/software-instrument/Learn_Prometheus_and_Grafana/demo-app$ make course/server
-go run cmd/course/main.go server start --config course/conf/server.yaml --migration course/migrations
-stat cmd/course/main.go: no such file or directory
-make: *** [Makefile:67: course/server] Error 1
-arthurhzna@arthurhzna-HYPE-5-AMD-X3:~/Downloads/grinding/software-instrument/Learn_Prometheus_and_Grafana/demo-app$ 
+package main
+
+import (
+	"github.com/imrenagicom/demo-app/cmd/course/commands"
+
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	err := commands.NewCommand().Execute()
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to start server")
+	}
+}
